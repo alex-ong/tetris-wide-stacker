8076c7085dd64f9e257d87d8b4bca1b86c706b07
alex-ong <the.onga@gmail.com>
2017-03-19 18:39:56 +1100

    Added main class. Working basic solution now.

---
+++ main.py
@@ -0,0 +1,56 @@
+'''
+Class that does the main loop that creates a layout.
+'''
+import TetrisField, TetrisPiece, FieldEvaluator
+
+class ShuffleMarker(object):
+    pass
+
+class LayoutCreator(object):
+    def __init__(self, field, onPlacePiece):
+        self.field = field
+        self.onPlacePiece = onPlacePiece
+        self.nextPieces = []
+        self.refillBag()
+    
+    def getPiece(self):        
+        result = self.nextPieces.pop(0)
+        
+        while isinstance(result, ShuffleMarker): #fix for endless loop
+            self.refillBag()
+            result = self.nextPieces.pop(0)
+        
+        return result
+    
+    def returnPiece(self, piece):
+        self.nextPieces.append(piece)
+                
+    def refillBag(self):
+        self.nextPieces.extend(TetrisPiece.getRandomBag())
+        self.nextPieces.append(ShuffleMarker())
+        
+    def createLayout(self):        
+        numPieces = int(self.field.width*self.field.height/4/2) #only fill half high
+        piecesPlaced = 0
+        while piecesPlaced < numPieces:        
+            piece = self.getPiece()            
+            
+            validPlacements = FieldEvaluator.listValidPlacements(self.field, piece)
+            finalPlacement = FieldEvaluator.evaluate(validPlacements)
+            
+            if finalPlacement is not None:                
+                self.field.placePiece(finalPlacement)
+                self.onPlacePiece()
+                piecesPlaced += 1    
+            else: #no placements, put the piece back into end of queue                
+                self.returnPiece(piece)
+            
+            
+def printField(field):
+    print(field)
+    print('\n')
+    
+if __name__ == '__main__':
+    field = TetrisField.TetrisField(100,15)
+    layout = LayoutCreator(field,lambda: printField(field))
+    layout.createLayout()
