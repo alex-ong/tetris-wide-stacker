00cbd923ff71d754a1e72b3e5fbbbf9da5603603
Alex Ong <the.onga@gmail.com>
2018-09-04 20:49:33 +1000

    Added flat skin.

--- fieldToImage/fieldToImage.py
+++ fieldToImage/fieldToImage.py
@@ -1,33 +1,27 @@
 from PIL import Image
 
 inputFile = 'layout.txt'
-outputFile = 'output.png'
+outputFile = 'output%0d.png'
 
+L_image = Image.open('l.png')
+J_image = Image.open('j.png')
+T_image = Image.open('t.png')
+O_image = Image.open('o.png')
+I_image = Image.open('i.png')
+S_image = Image.open('s.png')
+Z_image = Image.open('z.png')
+blank = None # Image.open('blank.png')
 
-if __name__ == '__main__':
-    lines = []
-    with open(inputFile, 'r') as f:
-        for line in f:
-            if len(line) > 2:
-                lines.append(line.strip())
-    L_image = Image.open('l.png')
-    J_image = Image.open('j.png')
-    T_image = Image.open('t.png')
-    O_image = Image.open('o.png')
-    I_image = Image.open('i.png')
-    S_image = Image.open('s.png')
-    Z_image = Image.open('z.png')
-    blank = None # Image.open('blank.png')
+lookup = { 'L': L_image,
+           'J': J_image,
+           'T': T_image,
+           'O': O_image,
+           'I': I_image,
+           'S': S_image,
+           'Z': Z_image,
+           '.': blank}
     
-    lookup = { 'L': L_image,
-               'J': J_image,
-               'T': T_image,
-               'O': O_image,
-               'I': I_image,
-               'S': S_image,
-               'Z': Z_image,
-               '.': blank}
-    
+def processLines(lines, number):
     canvas = Image.new('RGB', (len(lines[0]) * L_image.size[0], len(lines) * L_image.size[0]), color=0)
     
     tileWidth = L_image.size[0]
@@ -40,7 +34,23 @@ if __name__ == '__main__':
             if to_paste is not None:
                 canvas.paste(to_paste,offset)
     
-    canvas.save(outputFile)
+    canvas.save(outputFile % number)
+    
+if __name__ == '__main__':
+    number = 0
+    lines = []
+    with open(inputFile, 'r') as f:
+        for line in f:
+            if len(line) > 2:
+                lines.append(line.strip())
+            else:
+                if len(lines) > 4:
+                    processLines(lines, number)
+                    number += 1
+                    lines = []
+    
+
+    
             
         
     
